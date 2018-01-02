5c754c0b6f3c3d7499d5dd3b6f39afb092e11186
makecation <rachelkoukal@gmail.com>
2018-01-02 15:05:50 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cards[0]);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cards[2]);
+console.log("User flipped king");
+
+//THIS IS WHERE I GET STUCK
+if (cardsInPlay.length === 2) {
+} else if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
